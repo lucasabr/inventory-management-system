f9e1f05969e19820288767e84da1ee4bd709a8f2
lucasabr <lucasabr2001@gmail.com>
2021-07-21 23:52:03 -0400

    .

--- GUI.java
+++ GUI.java
@@ -1,14 +1,16 @@
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 import views.Menu;
 public class GUI {
     public GUI(){
         JFrame frame = new JFrame();
         JPanel menu = Menu.getInstance();
-        frame.add(menu);
+        frame.add(menu, BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
     }
-    public static void main() {
+    public static void main(String[] args) {
         new GUI();
     }
 }
