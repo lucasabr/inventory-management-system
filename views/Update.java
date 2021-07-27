035b84ce866b4f7aa01b8dbcf3c6c5ada7033509
lucasabr <lucasabr2001@gmail.com>
2021-07-26 23:20:01 -0400

    update page layout

--- views/Update.java
+++ views/Update.java
@@ -7,12 +7,38 @@ public class Update implements ActionListener, KeyListener{
     private JPanel updatePanel;
     private JFrame frame;
     private JPanel parent;
-
+    private JButton damages;
+    private JButton receive;
+    private JPanel createButtons(){
+        JPanel buttons = new JPanel();
+        buttons.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        buttons.setLayout(new GridLayout(1, 2));
+        receive = new JButton("Search Items");
+        damages = new JButton("Forms");
+        receive.addActionListener(this);
+        damages.addActionListener(this);
+        buttons.add(damages);
+        buttons.add(receive);
+        return buttons;
+    }
+    private void createLayout(JLabel title, JPanel buttons){
+        this.updatePanel = new JPanel();
+        this.updatePanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        this.updatePanel.add(title, c);
+        c.ipady=300;
+        c.ipadx=650;
+        c.gridy = 1;
+        this.updatePanel.add(buttons, c);
+    }
 
     private Update(){
-        JLabel text = new JLabel("Hello world");
+        JLabel text = new JLabel("Update Stock");
         updatePanel = new JPanel();
-        updatePanel.add(text);
+        JPanel buttons = createButtons();
+        createLayout(text, buttons);
         updatePanel.addKeyListener(this);
         updatePanel.setFocusable(true);
     }
